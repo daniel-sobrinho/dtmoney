d1f6f380e8f50effac8f4d4269666e93fa0bed64
Daniel Sobrinho <danielsobrihno.dev@gmail.com>
2023-03-26 00:22:46 -0300

    feat: lint code and install mirage js

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,35 +1,40 @@
-import { Container } from "./styles";
+import { useEffect } from 'react'
+import { Container } from './styles'
 
 export function TransactionsTable() {
-    return (
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  }, [])
 
-                <tbody>
-                <tr>
-                    <td>Desenvolvimento de Website</td>
-                    <td className="deposit">R$ 12.000</td>
-                    <td>Desenvolvimento</td>
-                    <td>20/02/2021</td>
-                </tr>
-                <tr>
-                    <td>Aluguel</td>
-                    <td className="withdraw">- R$ 1.100</td>
-                    <td>Casa</td>
-                    <td>17/02/2021</td>
-                </tr>
-            </tbody>
-            </table>
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
 
-            
-        </Container>
-    )
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td className="deposit">R$ 12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$ 1.100</td>
+            <td>Casa</td>
+            <td>17/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
 }
